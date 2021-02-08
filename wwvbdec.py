873fd733d998ae45186b97f6f0d36c8b02615ece
Jeff Epler <jepler@gmail.com>
2021-02-08 17:38:53 -0600

    Add wwvbdec
    wwvbdec.wwvbreceive is a stateful decoder of the amplitude portion of the
    wwvb signal implemented as a generator coroutine.
    
    Given a sequence consisting of the MARK of second 59 of a previous
    minute, plus the 60 seconds of the current minute, it will yield
    a WWVBTimecode.  At other times, it will yield None.
    
    The returned timecode will contain 60 items, even if the minute contains
    a leap second and there is nominally an extra MARK within the minute.
    
    Low level validation (must be mark / must be zero / DUT1 sign bits)
    is performed, but high level validation (e.g., day number is in range)
    is not.  I defend this decision because otherwise e.g., you must poke
    into this routine knowledge of what the WWVB epoch is so that you know
    whether year 00 has a day 366 or not.

---
+++ wwvbdec.py
@@ -0,0 +1,79 @@
+import wwvbgen
+
+# State 1: Unsync'd
+#  Marker: State 2
+#  Other: State 1
+# State 2: One marker
+#  Marker: State 3
+#  Other: State 1
+# State 3: Two markers
+#  Marker: State 3
+#  Other: State 4
+# State 4: Decoding a minute, starting in second 1
+#  Second
+
+always_zero = set((4, 10, 11, 14, 20, 21, 34, 35, 44, 54))
+
+
+def wwvbreceive():
+    minute = []
+    state = 1
+
+    value = yield None
+    while True:
+        # print(state, value, len(minute), "".join(str(int(i)) for i in minute))
+        if state == 1:
+            minute = []
+            if value == wwvbgen.AmplitudeModulation.MARK:
+                state = 2
+            value = yield None
+
+        elif state == 2:
+            if value == wwvbgen.AmplitudeModulation.MARK:
+                state = 3
+            else:
+                state = 1
+            value = yield None
+
+        elif state == 3:
+            if value != wwvbgen.AmplitudeModulation.MARK:
+                state = 4
+                minute = [wwvbgen.AmplitudeModulation.MARK, value]
+            value = yield None
+
+        elif state == 4:
+            minute.append(value)
+            if len(minute) % 10 == 0 and value != wwvbgen.AmplitudeModulation.MARK:
+                # print("MISSING MARK", len(minute), "".join(str(int(i)) for i in minute))
+                state = 1
+            elif len(minute) % 10 and value == wwvbgen.AmplitudeModulation.MARK:
+                # print("UNEXPECTED MARK")
+                state = 1
+            elif (
+                len(minute) - 1 in always_zero
+                and value != wwvbgen.AmplitudeModulation.ZERO
+            ):
+                # print("UNEXPECTED NONZERO")
+                state = 1
+            elif len(minute) == 60:
+                # print("FULL MINUTE")
+                tc = wwvbgen.WWVBTimecode(60)
+                tc.am[:] = minute
+                minute = []
+                state = 2
+                value = yield tc
+            else:
+                value = yield None
+
+
+if __name__ == "__main__":
+    timestamp = wwvbgen.WWVBMinuteIERS(1970, 1, 23, 59)
+    decoder = wwvbreceive()
+    next(decoder)
+    for i in range(3):
+        timecode = timestamp.as_timecode()
+        for code in timecode.am:
+            minute_maybe = decoder.send(code)
+            if minute_maybe:
+                assert minute_maybe.am == timecode.am[:60]
+        timestamp = timestamp.next_minute()
