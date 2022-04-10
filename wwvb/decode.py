d0d36790e08298201dcf56f25e4b27af5ef46ea9
Jeff Epler <jepler@gmail.com>
2022-04-10 11:54:16 -0500

    Update pylint, black

--- wwvb/decode.py
+++ wwvb/decode.py
@@ -22,9 +22,9 @@ import wwvb
 always_zero = set((4, 10, 11, 14, 20, 21, 34, 35, 44, 54))
 
 
-def wwvbreceive() -> Generator[
+def wwvbreceive() -> Generator[  # pylint: disable=too-many-branches
     Optional[wwvb.WWVBTimecode], wwvb.AmplitudeModulation, None
-]:  # pylint: disable=too-many-branches
+]:
     """A stateful decoder of WWVB signals"""
     minute: List[wwvb.AmplitudeModulation] = []
     state = 1
