6ad97731181247b905b7612f8e38a446ad09487b
Jeff Epler <jepler@gmail.com>
2022-10-08 11:45:11 -0500

    use bulletind, abandoned work

--- src/wwvb/updateiers.py
+++ src/wwvb/updateiers.py
@@ -15,6 +15,7 @@ import pathlib
 from typing import Callable, List, Optional
 
 import bs4
+import bulletind
 import click
 import platformdirs
 import requests
@@ -33,7 +34,6 @@ try:
 except (ImportError, NameError) as e:
     pass
 IERS_URL = "https://datacenter.iers.org/data/csv/finals2000A.all.csv"
-NIST_URL = "https://www.nist.gov/pml/time-and-frequency-division/atomic-standards/leap-second-and-ut1-utc-information"
 
 
 def update_iersdata(  # pylint: disable=too-many-locals, too-many-branches, too-many-statements
@@ -51,6 +51,7 @@ def update_iersdata(  # pylint: disable=too-many-locals, too-many-branches, too-
             offs = int(round(float(offs_str) * 10))
             if not offsets:
                 table_start = datetime.date(1858, 11, 17) + datetime.timedelta(jd)
+                # If the table starts after June 1, 1972 (it does), add the 1972 leap second
                 if table_start > datetime.date(1972, 6, 1):
                     when = datetime.date(1972, 6, 1)
                     while when < datetime.date(1972, 7, 1):
@@ -62,18 +63,6 @@ def update_iersdata(  # pylint: disable=too-many-locals, too-many-branches, too-
                     table_start = datetime.date(1972, 6, 1)
             offsets.append(offs)
 
-    wwvb_text = requests.get(NIST_URL).text
-    wwvb_data = bs4.BeautifulSoup(wwvb_text, features="html.parser")
-    wwvb_dut1_table = wwvb_data.findAll("table")[2]
-    assert wwvb_dut1_table
-    meta = wwvb_data.find("meta", property="article:modified_time")
-    assert isinstance(meta, bs4.Tag)
-    wwvb_data_stamp = (
-        datetime.datetime.fromisoformat(meta.attrs["content"])
-        .replace(tzinfo=None)
-        .date()
-    )
-
     def patch(patch_start: datetime.date, patch_end: datetime.date, val: int) -> None:
         off_start = (patch_start - table_start).days
         off_end = (patch_end - table_start).days
@@ -81,29 +70,26 @@ def update_iersdata(  # pylint: disable=too-many-locals, too-many-branches, too-
 
     wwvb_dut1: Optional[int] = None
     wwvb_start: Optional[datetime.date] = None
-    for row in wwvb_dut1_table.findAll("tr")[1:][::-1]:
-        cells = row.findAll("td")
-        when = datetime.datetime.strptime(cells[0].text, "%Y-%m-%d").date()
-        dut1 = cells[2].text.replace("s", "").replace(" ", "")
-        dut1 = int(round(float(dut1) * 10))
+    for data in bulletind.get_cached_bulletin_d_data():
+        when = data.start_date
+        dut1 = round(10 * data.dut1)
         if wwvb_dut1 is not None:
             assert wwvb_start is not None
             patch(wwvb_start, when, wwvb_dut1)
         wwvb_dut1 = dut1
         wwvb_start = when
 
-    # As of 2021-06-14, NIST website incorrectly indicates the offset of -600ms
-    # persisted through 2009-03-12, causing an incorrect leap second inference.
-    # Assume instead that NIST started broadcasting +400ms on January 1, 2009,
-    # causing the leap second to occur on 2008-12-31.
-    patch(datetime.date(2009, 1, 1), datetime.date(2009, 3, 12), 4)
-
-    # this is the final (most recent) wwvb DUT1 value broadcast.  We want to
-    # extend it some distance into the future, but how far?  We will use the
-    # modified timestamp of the NIST data.
-    assert wwvb_dut1 is not None
-    assert wwvb_start is not None
-    patch(wwvb_start, wwvb_data_stamp + datetime.timedelta(days=1), wwvb_dut1)
+    # this is the final (most recent) Bulletin D DUT1 value specified.  We
+    # might want to extend it some distance into the future, but how far?  The
+    # textual Bulletin D says something like "Bulletin D 136 should be issued
+    # in August 2017" or "Bulletin D 143 should be issued in 2023" but this is
+    # not in the xml format and thus not in the data from the bulletind
+    # package. Historically, bulletins have even been published AFTER their
+    # effective date. As such, transition immediately back to the IERS Bulletin
+    # A data immediately after the last Bulletin D data and do _NOT_ patch any
+    # additonal days. This is in contrast to previous behavior, where the NIST
+    # data was propagated until the reported modification date of the scraped
+    # web page.
 
     with open(target_file, "wt", encoding="utf-8") as output:
 
